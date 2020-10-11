25fcb474c9b30188c32d81c6da84be510dd48430
sagbeyeg <agbeyegbesisan@gmail.com>
2020-10-11 08:13:16 -0400

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     has_many :reservations, through: :listings
 
     has_many :trips, :class_name => :Reservation, :foreign_key => 'guest_id'
-    has_many :reviews, :through :trips
+    has_many :reviews, through: :trips
 end
